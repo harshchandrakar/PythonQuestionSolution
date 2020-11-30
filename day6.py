0e2bdcb4640b1c656d1e2e77d440fc414acd2f44
harsh kumar chandrakar <harshc252@gmail.com>
2020-11-30 13:00:33 +0530

    incomplete file day 6

---
+++ day6.py
@@ -0,0 +1,13 @@
+#question 18
+
+# import re
+# password = input('enter passwords :').split(',')
+# li =[]
+# for pa in password:
+#     if re.fullmatch(r"^.*(?=.{6,12})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", pa):
+#         li.append(pa)
+#
+# valid_pass = ','.join(li)
+# print(valid_pass)
+
+#question !9
