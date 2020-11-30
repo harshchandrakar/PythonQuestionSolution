7c85c6fe65906722e82b644e76ce97ff55ac5c74
harsh kumar chandrakar <harshc252@gmail.com>
2020-11-30 12:58:52 +0530

    part 2 of day 5

--- day5.py
+++ day5.py
@@ -10,3 +10,15 @@ print(squared_numbers)
 squared_number = [f'{int(num)**2}' for num in input('enter numbers: ').split(',') if int(num)%2 != 0]
 odd_squares = ','.join(squared_number)
 print(odd_squares)
+
+#question 17
+wd1 = input('start with D to deposit and W for withdrawl: ').split(',')
+D,W = 0,0
+for wd in wd1:
+    if wd.split(' ')[0] =='D':
+        D += int(wd.split(' ')[1])
+    elif wd.split(' ')[0]=='W':
+         W += int(wd.split(' ')[1])
+
+total = D - W
+print(f'total amount in bank : {total}')
